976350976220820949bee09bdb19a12cbe8bee59
Juliette3529 <Juliette@saint-jalme.fr>
2016-08-29 14:44:19 +0200

    ajout verification entrée texte

--- send_mail2.php
+++ send_mail2.php
@@ -1,4 +1,8 @@
 <?php
+if (    isset($_POST["mail"])
+        AND isset($_POST["mail"])
+        AND isset($_POST["mail"]) )
+{
 $adresse = $_POST["mail"]; 
 $objet = $_POST["objet"]; 
 $message = $_POST["message"]; 
@@ -7,7 +11,9 @@ $headers = 'From: juliette@saint-jalme.fr ' . "\r\n" .
      'X-Mailer: PHP/' . phpversion();
 mail($adresse, $objet, $message, $headers);
 header('Location: send_mail3.php');
+}
+else {
+   echo '<h2>ECHEC</h2>';;
+};
 
-/* [Mon Aug 29 10:43:52.891912 2016] [:error] [pid 8078] [client 127.0.0.1:42040] PHP Notice:  Undefined index: mail in /var/www/html/php_challenge1_send_mail/send_mail2.php on line 2, referer: http://localhost/php_challenge1_send_mail/send_mail.php
-*/
 ?>
